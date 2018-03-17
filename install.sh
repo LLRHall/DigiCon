b30f960c24597fa95621d44cdff74d3d58cb978a
Himanshu Mishra <himanshu2014iit@gmail.com>
2018-03-17 18:27:03 +0530

    Add installation script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PROXY=http://172.16.2.30:8080
+
+sudo add-apt-repository universe
+sudo -E apt-get update
+sudo -E apt-get install build-essential -y
+sudo -E apt-get install python-dev -y
+sudo -E apt-get install python-setuptools -y
+sudo -E apt-get install python-pip -y
+sudo pip --proxy=$PROXY install flask
+
