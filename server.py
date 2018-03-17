6344f259c4febbf904724b9a9abf59d2d5ccdc82
Himanshu Mishra <himanshu2014iit@gmail.com>
2018-03-17 17:20:55 +0530

    Open browser when the server starts

--- server.py
+++ server.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template,session,request,redirect
+#!/usr/bin/env python
+
+import threading
+import webbrowser
+from flask import Flask, render_template, session, request, redirect
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -22,5 +27,16 @@ def feedback():
     return render_template('feedback.html')
 
 
-app.debug = True
-app.run()
+if __name__ == '__main__':
+    app.debug = True
+    port = 8000
+    url = "http://127.0.0.1:{0}".format(port)
+    print(""" * DigiCon v1.0 is running on port 8000\n
+        Please open {0} in your browser to use the software.
+        """.format(url))
+
+    if not app.debug:
+        threading.Timer(1.00, lambda: webbrowser.open(url) ).start()
+
+    app.run(port=port)
+
